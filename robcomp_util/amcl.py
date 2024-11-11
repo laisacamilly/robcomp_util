f00eae9d68702512da051b315417dcdeb2d3f824
dsoldev <dpsoler09@gmail.com>
2024-11-11 16:27:27 -0300

    fix

--- robcomp_util/amcl.py
+++ robcomp_util/amcl.py
@@ -86,7 +86,11 @@ class AMCL(): # Mude o nome da classe
 
                     self.x = x_map + delta_x
                     self.y = y_map + delta_y
-                    self.yaw = yaw_map + self.odom_yaw
+                    self.yaw = np.arctan2(
+                        math.sin(yaw_map + self.odom_yaw),
+                        math.cos(yaw_map + self.odom_yaw)
+                    )
+
                     self.get_logger().info(f'Current pose: ({self.x:.2f}, {self.y:.2f})')
 
                     self.odom_ready = True
